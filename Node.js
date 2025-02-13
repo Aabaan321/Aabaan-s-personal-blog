fe8a04fc865005d3f23417cf2034fe5cd7522dd3
Aabaan321 <aabaanrahil@gmail.com>
2025-02-14 00:06:17 +0400

    Update Node.js

--- Node.js
+++ Node.js
@@ -12,7 +12,7 @@ app.post('/chat', async (req, res) => {
 
     try {
         // Make the request to OpenAI API
-        const response = await fetch('', {
+        const response = await fetch('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', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
